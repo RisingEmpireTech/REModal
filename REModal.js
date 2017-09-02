0021ee0768c07c1c0316e1171d40df508f5650f0
Blake Buck <blakebuck@users.noreply.github.com>
2017-09-02 17:44:21 -0400

    Update REModal.js

--- REModal.js
+++ REModal.js
@@ -1,15 +1,19 @@
-/**************************************************************************************** 
+/* global Plastic, $, jQuery */
+
+/***************************************************************************************** 
  * 
  *  Copyright (C) Rising Empire Technologies, Inc - All Rights Reserved
  *  Unauthorized use, modification or distribution of this work is strictly prohibited.
+ * 
  *  Written by:
+ *  Brendan Tackney <brendan@risingempire.tech>, March 2017
+ *  Sheldon Pasciak <sheldon@risingempire.tech>, March 2017
  *  Blake Buck <blake@risingempire.tech>, March 2017
  *
  ****************************************************************************************/
+function PlasticModal(){ var AllRightsReserved = ' Rising Empire Technologies, Inc. '}
 
-/* global $*/ // This is for cloud9 editor
-
-var Modal = {
+Plastic.modal = {
     modalContent: null,
     modalHeight: 0,
     modalWidth: 0,
@@ -22,94 +26,128 @@ var Modal = {
         openSpeed: 700,
         prefix: "REModal-"
     },
+    /**
+     * Centers the content of the modal examining window height/width and model height/width
+     * 
+     */    
     centerContent: function(){
         var viewportHeight = $(window).height();
         var viewportWidth = $(window).width();       
         
-        Modal.modalHeight = Math.max(Modal.modalContent.height(), Modal.modalHeight);
-        Modal.modalWidth = Math.max(Modal.modalContent.width(), Modal.modalWidth);
+        Plastic.modal.modalHeight = Math.max(Plastic.modal.modalContent.height(), Plastic.modal.modalHeight);
+        Plastic.modal.modalWidth = Math.max(Plastic.modal.modalContent.width(), Plastic.modal.modalWidth);
 
-        var left = Modal.modalWidth >= viewportWidth ? 0 : (viewportWidth - Modal.modalWidth) / 2;
-        var top = Modal.modalHeight >= viewportHeight ? 0 : (viewportHeight - Modal.modalHeight) / 2;
+        var left = Plastic.modal.modalWidth >= viewportWidth ? 0 : (viewportWidth - Plastic.modal.modalWidth) / 2;
+        var top = Plastic.modal.modalHeight >= viewportHeight ? 0 : (viewportHeight - Plastic.modal.modalHeight) / 2;
         
-        Modal.modalContent.css("left", left);
-        Modal.modalContent.css("top", top);
-        Modal.modalContent.css("max-height", viewportHeight);
-        Modal.modalContent.css("max-width", viewportWidth);
+        Plastic.modal.modalContent.css("left", left);
+        Plastic.modal.modalContent.css("top", top);
+        Plastic.modal.modalContent.css("max-height", viewportHeight);
+        Plastic.modal.modalContent.css("max-width", viewportWidth);
         
-        Modal.modalOverlay.height(viewportHeight);
+        Plastic.modal.modalOverlay.height(viewportHeight);
     },
     close: function(){
-        Modal.modalContent.slideUp(Modal.options.closeSpeed);
-        Modal.modalOverlay.slideUp(Modal.options.closeSpeed);
+        Plastic.modal.modalContent.slideUp(Plastic.modal.options.closeSpeed);
+        Plastic.modal.modalOverlay.slideUp(Plastic.modal.options.closeSpeed);
         
-        if (Modal.options.callback != null){            
-            Modal.options.callback(Modal.options.callbackObj);
+        if (Plastic.modal.options.callback != null){            
+            Plastic.modal.options.callback(Plastic.modal.options.callbackObj);
         }
         
-        if (Modal.options.destroyOnClose){
-            Modal.destroy();
+        if (Plastic.modal.options.destroyOnClose){
+            Plastic.modal.destroy();
         }
     },
-    create: function(htmlContent, options){
-        Modal.modalHeight = 0;
-        Modal.modalWidth = 0;
-        $.extend(Modal.options, options);
-        
-        var contentDivId = Modal.options.prefix + "Content";
-        var overlayDivId = Modal.options.prefix + "Overlay";
-        
-        if ($("#" + overlayDivId).length <= 0){
-            $('<div>').attr({ 
-                id: contentDivId,
-                class: "REModal-Content"
-            }).prependTo('body');
-            $('<div>').attr({ 
-                id: overlayDivId,
-                class: "REModal-Overlay"
-            }).prependTo('body');
+    /**
+          ```js
+                  
+            Modal.create(html, {destroyOnClose: true, height: '450px', width: '800px', callback: null});
+            
+            Modal.show();
+          
+          ``` 
+     */     
+    create: function(htmlContent, options, creationCallback){
+        if (htmlContent === null && typeof options.contentURI !== "undefined"){
+            if (typeof creationCallback === "undefined"){
+                console.warn("You are creating an async modal, without a creationCallback.");
+            }
+            $.get(options.contentURI, function(html){
+                htmlContent = html;
+                create();
+            });
+        }
+        else {
+            create();
         }
         
-        Modal.modalContent = $("#" + contentDivId);
-        Modal.modalOverlay = $("#" + overlayDivId);
-    
-        Modal.setOptionalDimensions();
-    
-        Modal.modalContent.html(htmlContent);
-
-        Modal.modalOverlay.click(Modal.hide);
+        function create(){
+            Plastic.modal.modalHeight = 0;
+            Plastic.modal.modalWidth = 0;
+            $.extend(Plastic.modal.options, options);
+            
+            var contentDivId = Plastic.modal.options.prefix + "Content";
+            var overlayDivId = Plastic.modal.options.prefix + "Overlay";
+            
+            if ($("#" + overlayDivId).length <= 0){
+                $('<div>').attr({ 
+                    id: contentDivId,
+                    class: "REModal-Content"
+                }).prependTo('body');
+                $('<div>').attr({ 
+                    id: overlayDivId,
+                    class: "REModal-Overlay"
+                }).prependTo('body');
+            }
+            
+            Plastic.modal.modalContent = $("#" + contentDivId);
+            Plastic.modal.modalOverlay = $("#" + overlayDivId);
         
-        Modal.centerContent();
-        $(window).resize(Modal.centerContent);
+            Plastic.modal.setOptionalDimensions();
+        
+            Plastic.modal.modalContent.html(htmlContent);
+    
+            Plastic.modal.modalOverlay.click(Plastic.modal.hide);
+            
+            Plastic.modal.centerContent();
+            
+            $(window).resize(Plastic.modal.centerContent);
+            
+            if (typeof creationCallback !== "undefined"){
+                creationCallback();    
+            }
+        }
     },
     destroy: function(){
-        Modal.options.callback = function(callbackObj){};
-        Modal.options.callbackObj = null;
-        Modal.options.closeSpeed = 500;
-        Modal.options.destroyOnClose = false;
-        Modal.options.openSpeed = 700;
-        Modal.options.prefix = "REModal-";
-        Modal.modalContent.remove();
-        Modal.modalOverlay.remove();
+        Plastic.modal.options.callback = function(callbackObj){};
+        Plastic.modal.options.callbackObj = null;
+        Plastic.modal.options.closeSpeed = 500;
+        Plastic.modal.options.destroyOnClose = false;
+        Plastic.modal.options.openSpeed = 700;
+        Plastic.modal.options.prefix = "REModal-";
+        Plastic.modal.modalContent.remove();
+        Plastic.modal.modalOverlay.remove();
     },
     hide: function(){
         // Alias for close
-        Modal.close();
+        Plastic.modal.close();
     },
     open: function(){
-        Modal.modalOverlay.slideDown(Modal.options.openSpeed);
-        Modal.modalContent.slideDown(Modal.options.openSpeed);
+        Plastic.modal.modalOverlay.slideDown(Plastic.modal.options.openSpeed);
+        Plastic.modal.modalContent.slideDown(Plastic.modal.options.openSpeed);
     },
     setOptionalDimensions: function(){
-        if (typeof Modal.options.height !== "undefined"){
-            Modal.modalContent.css("height", Modal.options.height);
+        if (typeof Plastic.modal.options.height !== "undefined"){
+            Plastic.modal.modalContent.css("height", Plastic.modal.options.height);
         }
-        if (typeof Modal.options.width !== "undefined"){
-            Modal.modalContent.css("width", Modal.options.width);
+        if (typeof Plastic.modal.options.width !== "undefined"){
+            Plastic.modal.modalContent.css("width", Plastic.modal.options.width);
         }
     },
     show: function(){
         // Alias for open
-        Modal.open();
+        Plastic.modal.open();
     }
-};
+}
+Modal = Plastic.modal;
